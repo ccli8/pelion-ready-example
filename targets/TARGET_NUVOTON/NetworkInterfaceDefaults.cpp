11496e449d3fa03e68f88d1be9594b5e0e4d11d5
ccli8 <ccli8@nuvoton.com>
2019-03-20 14:44:25 +0800

    Support NUMAKER_PFM_M2351 for pelion client
    1. Override M2351 memory partition and provide custom secure library
       ROM partition: secure 64KiB/non-secure 448KiB
       RAM partition: secure 8KiB/non-secure 88KiB
    2. Remove M2351 default secure library from compile list (.mbedignore)
    3. Provide bootloader:
       (1) NUSD for UC
       (2) NVStore (SOTP) RoT
    4. Override network interface using on-board ESP8266 WiFi module (targets/
       TARGET_NUVOTON/NetworkInterfaceDefaults.cpp).
    5. Override system storage using NUSD (drivers/storage/MySystemStorage.cpp).
    6. With RoT, bootloader and client application must match:
       (1) Insecure (fake) RoT:
           Bootloader/mbed_app.json: ARM_BOOTLOADER_USE_NVSTORE_ROT == 0
           Client application: PAL_USE_HW_ROT == 1 (simple-mbed-cloud-client/simple-mbed-cloud-client/pal_plat_rot_insecure.c)
       (2) NVStore (SOTP) RoT:
           Bootloader/mbed_app.json: ARM_BOOTLOADER_USE_NVSTORE_ROT == 1
           Client application: PAL_USE_HW_ROT == 0

--- targets/TARGET_NUVOTON/NetworkInterfaceDefaults.cpp
+++ targets/TARGET_NUVOTON/NetworkInterfaceDefaults.cpp
@@ -1,5 +1,7 @@
-/* Network interface defaults
- * Copyright (c) 2018-2020 Nuvoton
+/*
+ * Copyright (c) 2019-2020, Nuvoton Technology Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "mbed.h"
 
 #define ETHERNET 1
@@ -20,18 +23,126 @@
 #define MESH 3
 #define CELLULAR 4
 
-#if MBED_CONF_TARGET_NETWORK_DEFAULT_INTERFACE_TYPE == ETHERNET
-
-#include "EthInterface.h"
-
 EthInterface *EthInterface::get_default_instance()
 {
     return get_target_default_instance();
 }
 
+WiFiInterface *WiFiInterface::get_default_instance()
+{
+    return get_target_default_instance();
+}
+
+#include "ESP8266Interface.h"
+
+#define ESP8266_AT_ONBOARD      1   // On-board ESP8266
+#define ESP8266_AT_EXTERN       2   // External ESP8266
+
+#ifndef ESP8266_AT_SEL
+#warning "ESP8266_AT_SEL missing. Defaults to ESP8266_AT_ONBOARD."
+#define ESP8266_AT_SEL          ESP8266_AT_ONBOARD
+#endif
+
+WiFiInterface *WiFiInterface::get_target_default_instance()
+{
+    /* ESP8266Interface(TX, RX, debug, RTS, CTS, RST); */
+
+#if ESP8266_AT_SEL == ESP8266_AT_ONBOARD
+
+    #if TARGET_NUMAKER_IOT_M487
+    static ESP8266Interface esp(PH_8, PH_9, false, NC, NC, PH_3);
+
+    #elif TARGET_NUMAKER_PFM_M2351
+    static DigitalOut esp_pwr_off(PD_7, 1);     // Disable power to on-board ESP8266
+    static ESP8266Interface esp(PD_1, PD_0, false, PD_3, PD_2, NC);
+
+    if ((int) esp_pwr_off) {                    // Turn on on-board ESP8266
+        wait_ms(50);
+        esp_pwr_off = 0;
+        wait_ms(50);
+    }
+
+    #else
+    static ESP8266Interface esp;
+
+    #endif
+
+#elif ESP8266_AT_SEL == ESP8266_AT_EXTERN
+
+    #if TARGET_NUMAKER_PFM_NANO130
+    static ESP8266Interface esp(D1, D0, false, PB_6, PB_7, D2);
+
+    #elif TARGET_NUMAKER_PFM_NUC472
+    static ESP8266Interface esp(PD_15, PF_0, false, PD_14, PD_13, D2);
+
+    #elif TARGET_NUMAKER_PFM_M453
+    static ESP8266Interface esp(A3, A2, false, A5, A4, D2);
+
+    #elif TARGET_NUMAKER_PFM_M487 || TARGET_NUMAKER_IOT_M487
+    static ESP8266Interface esp(D1, D0, false, A2, A3, D2);
+
+    #elif TARGET_NUMAKER_PFM_M2351
+    static ESP8266Interface esp(D1, D0, false, A3, A2, D2);
+
+    #else
+    static ESP8266Interface esp;
+
+    #endif
+
+#endif
+
+    return &esp;
+}
+
+void WiFiInterface::set_default_parameters()
+{
+#ifdef MBED_CONF_NSAPI_DEFAULT_WIFI_SSID
+#ifndef MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD
+#define MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD NULL
+#endif
+#ifndef MBED_CONF_NSAPI_DEFAULT_WIFI_SECURITY
+#define MBED_CONF_NSAPI_DEFAULT_WIFI_SECURITY NONE
+#endif
+#define concat_(x,y) x##y
+#define concat(x,y) concat_(x,y)
+#define SECURITY concat(NSAPI_SECURITY_,MBED_CONF_NSAPI_DEFAULT_WIFI_SECURITY)
+    set_credentials(MBED_CONF_NSAPI_DEFAULT_WIFI_SSID, MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD, SECURITY);
+#endif
+}
+
+void NetworkInterface::set_default_parameters()
+{
+
+}
+
 NetworkInterface *NetworkInterface::get_default_instance()
 {
+    return get_target_default_instance();
+}
+
+#if MBED_CONF_TARGET_NETWORK_DEFAULT_INTERFACE_TYPE == ETHERNET
+MBED_WEAK NetworkInterface *NetworkInterface::get_target_default_instance()
+{
     return EthInterface::get_default_instance();
+}
+
+#elif MBED_CONF_TARGET_NETWORK_DEFAULT_INTERFACE_TYPE == WIFI
+NetworkInterface *NetworkInterface::get_target_default_instance()
+{
+    /* WiFi is not expected to work unless we have configuration parameters.
+     * We do not hook up to WifiInterface::get_default_instance() unless
+     * we have at least an access point name.
+     */
+#ifdef MBED_CONF_NSAPI_DEFAULT_WIFI_SSID
+    WiFiInterface *wifi = WiFiInterface::get_default_instance();
+    if (!wifi) {
+        return NULL;
+    }
+    wifi->set_default_parameters();
+    return wifi;
+#else
+    return NULL;
+#endif
 }
 
 #endif
